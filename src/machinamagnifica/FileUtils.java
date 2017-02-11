d370053554f1908315f3377b0a0abaebf231ee47
Alexandre GC <a.gaspardcilia@gmail.com>
2017-02-11 19:00:01 +0100

    Nettoyage...

--- src/machinamagnifica/FileUtils.java
+++
@@ -1,8 +0,0 @@
-package machinamagnifica;
-
-import java.io.File;
-import java.util.Scanner;
-
-public class FileUtils {
-	
-}
--- src/machinamagnifica/InvalidDataException.java
+++ src/machinamagnifica/InvalidDataException.java
@@ -2,4 +2,7 @@ package machinamagnifica;
 
 public class InvalidDataException extends Exception {
 
+	private static final long serialVersionUID = 6319175537117866865L;
+
+
 }
